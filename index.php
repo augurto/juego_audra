24abacde19d086e59951b786c0e982024baf1c83
Ego <augurto.17@gmail.com>
2022-01-23 13:23:49 -0500

    otro menu 2

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+<html>
+<head>	
+	<meta charset="utf-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1">
+		<title>MenU</title>
+		<link rel="stylesheet" type="text/css" href="css/menu.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <script src="js/archivo.js"></script>
 </head>
 <body>
-	<header>
-      <input type="checkbox" id="check-ok">
-    <label for="check-ok"><div class="navfake"></div></label>
-
-		<nav class="hide">
-			<ul class="hide">
-				<li class="hide"><a class="navegacion" href="#">Inicio</a></li>
-				<li class="hide"><a class="navegacion" href="#">Works</a></li>
-				<li class="hide"><a class="navegacion" href="#">Blog</a></li>
-				<li class="hide"><a class="navegacion" href="#">Contact</a></li>
+	<div class="sidebar">
+		<div class="logo"><h4>LOGO</h4></div>
+		<nav>
+			<ul>
+				<li>
+				<a href="#">Home</a>
+				<ul class="cap_2">
+					<li>1QUE</li>
+					<li>2QUE</li>
+					<li>3QUE</li>
+				</ul>
+				</li>
+				<li><a href="#">Daxua</a></li>
+				<li><a href="#">Teemo</a></li>
+				<li><a href="#">Fizz</a></li>
+				<li><a href="#">Sona</a></li>
+				<li><a href="#">Soraka</a></li>
 			</ul>
+			<div class="icon">
+				<i class="fas fa-bars"></i>
+			</div>
 		</nav>
-	</header>
-  <main>
-    <img src="http://bikeexperiencetenerife.com/img/content/home/slide2.jpg" alt="" />
-    <h1>Bike Experience Tenerife</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, temporibus officiis est. Suscipit cumque adipisci eaque nulla consectetur! Architecto quisquam sed fugiat nam doloremque, laborum ducimus voluptas fuga, quaerat aspernatur?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, temporibus officiis est. Suscipit cumque adipisci eaque nulla consectetur! Architecto quisquam sed fugiat nam doloremque, laborum ducimus voluptas fuga, quaerat aspernatur?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, temporibus officiis est. Suscipit cumque adipisci eaque nulla consectetur! Architecto quisquam sed fugiat nam doloremque, laborum ducimus voluptas fuga, quaerat aspernatur?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, temporibus officiis est. Suscipit cumque adipisci eaque nulla consectetur! Architecto quisquam sed fugiat nam doloremque, laborum ducimus voluptas fuga, quaerat aspernatur?</p>
-  </main>
-</body>
+	</div>
 </html>
