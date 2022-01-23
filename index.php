81dc56c6927d45406f322947bb88319875ea01f4
Ego <augurto.17@gmail.com>
2022-01-23 14:45:43 -0500

    XD menu en php

--- index.php
+++ index.php
@@ -3,33 +3,11 @@
 <head>	
 	<meta charset="utf-8">
 		<meta name="viewport" content="width = device-width, initial-scale = 1">
-		<title>Puto el que lo lea</title>
+		<title>Puto el que lo lea...</title>
 		<link rel="stylesheet" type="text/css" href="css/menu.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <script src="js/archivo.js"></script>
 </head>
 <body style="background-color: #2a2b2c;">
-	<div class="sidebar">
-		<div class="logo"><h4>Audra</h4></div>
-		<nav>
-			<ul>
-				<li>
-				<a href="#">Inicio</a>
-				<ul class="cap_2">
-					<li>link1.1</li>
-					<li>link1.2</li>
-					<li>link1.3</li>
-				</ul>
-				</li>
-				<li><a href="#">link1</a></li>
-				<li><a href="#">link2</a></li>
-				<li><a href="#">link3</a></li>
-				<li><a href="#">link4</a></li>
-				<li><a href="#">link5</a></li>
-			</ul>
-			<div class="icon">
-				<i class="fas fa-bars"></i>
-			</div>
-		</nav>
-	</div>
+    <?php include 'menu.php';?> 
 </html>
---
+++ menu.php
@@ -0,0 +1,23 @@
+<div class="sidebar">
+		<div class="logo"><h4>Audra</h4></div>
+		<nav>
+			<ul>
+				<li>
+				<a href="#">Inicio</a>
+				<ul class="cap_2">
+					<li>link1.1</li>
+					<li>link1.2</li>
+					<li>link1.3</li>
+				</ul>
+				</li>
+				<li><a href="#">link1...</a></li>
+				<li><a href="#">link2</a></li>
+				<li><a href="#">link3</a></li>
+				<li><a href="#">link4</a></li>
+				<li><a href="#">link5</a></li>
+			</ul>
+			<div class="icon">
+				<i class="fas fa-bars"></i>
+			</div>
+		</nav>
+	</div>
