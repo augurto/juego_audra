dd5dab1ba83a63510f14d9cc253424f8a954e7da
Ego <augurto.17@gmail.com>
2022-01-23 14:23:27 -0500

    ...

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>	
 	<meta charset="utf-8">
 		<meta name="viewport" content="width = device-width, initial-scale = 1">
-		<title>MenU</title>
+		<title>Puto el que lo lea</title>
 		<link rel="stylesheet" type="text/css" href="css/menu.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <script src="js/archivo.js"></script>
