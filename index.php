aa1d475f1a732ab1336bf5ddf30dd396ff710853
Ego <augurto.17@gmail.com>
2022-01-23 14:20:59 -0500

    cambiando el color del bodey desde el html

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <script src="js/archivo.js"></script>
 </head>
-<body>
+<body style="background-color: #192433;">
 	<div class="sidebar">
 		<div class="logo"><h4>Audra</h4></div>
 		<nav>
